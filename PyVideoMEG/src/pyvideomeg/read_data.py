4ba205e80998a17f76e18421295f8406ab070ab2
Andrey Zhdanov <andrey.zhdanov@aalto.fi>
2014-10-01 11:30:51 +0300

    Added copyright notices

--- PyVideoMEG/src/pyvideomeg/read_data.py
+++ PyVideoMEG/src/pyvideomeg/read_data.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 21 16:07:07 2012
+    Copyright (C) 2014 BioMag Laboratory, Helsinki University Central Hospital
 
-@author: andrey
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import struct
