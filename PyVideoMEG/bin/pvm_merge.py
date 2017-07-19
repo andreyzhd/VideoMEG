780c4602343d5de69e04b3b5d4b7dce0c2129065
Manezki <manezki@gmail.com>
2017-07-19 17:06:53 +0300

    Add StringIO compatibility between 2.* and 3.*

--- PyVideoMEG/bin/pvm_merge.py
+++ PyVideoMEG/bin/pvm_merge.py
@@ -30,7 +30,10 @@ import shutil
 import numpy
 
 from PIL import Image, ImageDraw, ImageFont
-import StringIO
+try:                    #Python2.*
+    from StringIO import StringIO
+except ImportError:     #Python3.*
+    from io import StringIO
 
 import pyvideomeg
 
@@ -69,8 +72,8 @@ while i < len(vid_file_1.ts):
     indx.append(numpy.argmin(abs(vid_file_1.ts[i] - vid_file_2.ts)))
     err.append(vid_file_1.ts[i] - vid_file_2.ts[indx[-1]])
     
-    img1 = Image.open(StringIO.StringIO(vid_file_1.get_frame(i)))
-    img2 = Image.open(StringIO.StringIO(vid_file_2.get_frame(indx[-1])))
+    img1 = Image.open(StringIO(vid_file_1.get_frame(i)))
+    img2 = Image.open(StringIO(vid_file_2.get_frame(indx[-1])))
     
     draw = ImageDraw.Draw(img1)
     draw.text((10,0), '%i  :  %s' % (vid_file_1.ts[i], pyvideomeg.ts2str(vid_file_1.ts[i])), font=fnt, fill='black')
