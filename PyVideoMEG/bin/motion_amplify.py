37bf5faf09980a6c72ae1e79113385395cf215df
Manezki <manezki@gmail.com>
2017-07-19 13:59:18 +0300

    Add motion amplify files

---
+++ PyVideoMEG/bin/motion_amplify.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env -tt
+
+# If Matlab.engine is not imported before numpy, it will crash and inform that installation is corrupted.
+# This is not reported to MathWorks as of yet.
+from __future__ import print_function
+
+import matlab.engine
+import tempfile
+import subprocess
+import sys
+import shutil
+# TODO Check compatibility for 2.7. Was changed from StringIO to io.StringIO
+import io.StringIO
+import pyvideomeg
+from PIL import Image
+from io import BytesIO
+from os import remove, path as op
+from math import ceil
+# TODO Should we list all the necessary packages to run all the funcitonality?
+from scipy.io import loadmat
+
+
+class OverLappingEvents(Exception):
+    """
+    Raised when overlapping events would produce ambiguous amplification.
+    """
+    pass
+
+
+class NonMatchingAmplificaton(Exception):
+    """
+    Raised when original and amplified file, don't match according to
+    Elektas variables.
+    """
+    pass
+
+def _rounded_evl_list(event_list):
+    """
+    Returns list of (start, end) -timestamps that have been ceiled to
+    closest 2 second blocks.
+    """
+    listed = []
+    for event in event_list.events:
+        middle = event.time + (event.duration / 2)
+        ceiled_duration = float(ceil(event.duration))
+        if ceiled_duration % 2 != 0:
+            ceiled_duration = ceiled_duration + 1
+        listed.append((middle - ceiled_duration / 2, middle + ceiled_duration / 2))
+    return listed
+
+def phase_based_amplification(video_file, sample_count, frames_per_sample, engine):
+    """
+    Calls matlab script with propier parameters, to perform amplification on video_file.
+    Matlab saves the resulting video as matrix to /tmp/vid.mat.
+    """
+    cycles = 1
+    print("SAMPLES: " + str(sample_count))
+    print("FRAMES: " + str(frames_per_sample))
+    print("Invoking amplify.m")
+    # TODO Add option for multiple cycles
+    # TODO Change amplify.m to handle multiple cycles
+    # TODO Test above functionalities
+    amplified_as_matrix = engine.amplify(video_file, sample_count,
+                                         frames_per_sample, cycles, nargout=0)
+    return amplified_as_matrix
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        assert sys.argv[-1] > 4 or sys.argv[-4:] != ".fif"
+
+        FIF = op.split(sys.argv[1])[1]
+        TREE = op.split(sys.argv[1])[0]
+        FNAME = FIF[:-4]
+        VIDEO_FILE = op.join(TREE, FNAME + ".video.dat")
+
+        try:
+            FIF = pyvideomeg.read_data.FifData(op.join(TREE, FNAME + ".fif"))
+        except IOError:
+            print(".fif file was not found. Cannot get accurate timing data - exiting.")
+            sys.exit()
+        try:
+            EVL = pyvideomeg.read_data.EvlData(op.join(TREE, FNAME + ".evl"))
+        except IOError:
+            print(".evl file was not found. Nothing to amplify - exiting.")
+            sys.exit()
+        EVENT_LIST = _rounded_evl_list(EVL)
+
+        # Check for overlaps in events
+        for i in range(1, len(EVENT_LIST)):
+            if EVENT_LIST[i][0] < EVENT_LIST[i-1][1]:
+                raise OverLappingEvents("Events " + str(i-1) + " and " + str(i) + " overlap.\n" +
+                                        "Amplification would be ambiguous")
+
+
+        ORIGINAL = pyvideomeg.VideoData(VIDEO_FILE)
+        AMPLIFIED = pyvideomeg.VideoFile(op.join(TREE, FNAME + ".video.amp.dat"), ORIGINAL.ver,
+                                         site_id=ORIGINAL.site_id, is_sender=ORIGINAL.is_sender)
+
+        FPS = (len(ORIGINAL.ts)-1) * 1000. / (ORIGINAL.ts[-1] - ORIGINAL.ts[0])
+
+
+        # Start Matlab-engine for amplification
+        # Won't work if internet connection is not available - matlab cannot check license.
+        try:
+            _ENG = matlab.engine.start_matlab()
+        except:
+            print("Problem starting Matlab engine. Possible reasons for this include:\n" +
+                  " 1 - Is your matlab engine for python installed and accessible?\n" +
+                  " 2 - Do you have internet connection, to check your matlab license?\n\n" +
+                  "Cannot proceed with amplification. Cleaning up and exiting.")
+            remove(op.join(TREE, FNAME + ".video.amp.dat"))
+            sys.exit()
+
+
+        EVENT_NUMBER = 0
+        i = 0
+
+        while i < len(ORIGINAL.ts):
+            FRAME = ORIGINAL.get_frame(i)
+            # TODO VIDEO_TIME points to wrong time.
+            # Evl marks the time from the start of the fif file, which differs from the time
+            # calculated from video timestamps.
+            # Evl matching time stamp can be found from .fif file. MNE-python can extract that
+            # information.
+            VIDEO_TIME = (ORIGINAL.ts[i] - FIF.start_time)/1000.0
+            # All events handled or first event hasn't started yet
+            if EVENT_NUMBER >= len(EVENT_LIST) or VIDEO_TIME < EVENT_LIST[EVENT_NUMBER][0]:
+                AMPLIFIED.append_frame(ORIGINAL.ts[i], FRAME)
+                i = i + 1
+            # Amplify event
+            elif (VIDEO_TIME >= EVENT_LIST[EVENT_NUMBER][0] and
+                  VIDEO_TIME <= EVENT_LIST[EVENT_NUMBER][1]):
+                TMP_FLDR = tempfile.mkdtemp()
+                j = i
+                k = 0
+                # Read the whole event and save on temporary folder
+                while (ORIGINAL.ts[j] - ORIGINAL.ts[0])/1000.0 <= EVENT_LIST[EVENT_NUMBER][1]:
+                    IMG = Image.open(io.StringIO(ORIGINAL.get_frame(j)))
+                    IMG.save('%s/%06i.jpg' % (TMP_FLDR, k))
+                    j = j + 1
+                    k = k + 1
+
+                FFMPEG = subprocess.Popen(["ffmpeg", "-r", str(FPS)+"/1", "-i",
+                                           TMP_FLDR+"/%06d.jpg", "-c:v", "mjpeg", "-q:v",
+                                           "0", TMP_FLDR+"/vid.avi"])
+                FFMPEG.wait()
+
+                SAMPLE_COUNT = round((EVENT_LIST[EVENT_NUMBER][1] -
+                                      EVENT_LIST[EVENT_NUMBER][0]) / 2.)
+                FRAME_PER_SAMPLE = round(float(k) / SAMPLE_COUNT)
+                phase_based_amplification(op.join(TMP_FLDR, "vid.avi"), SAMPLE_COUNT,
+                                          FRAME_PER_SAMPLE, _ENG)
+                _ENG.clear('all', nargout=0)
+                AMPLIFIED_VERSION = loadmat("/tmp/vid.mat")['out']
+
+                # Write amplified event to video.amp.dat.
+                # k was treated as a index before.
+                for indx in range(k):
+                    IMG = Image.fromarray(AMPLIFIED_VERSION[:, :, :, indx])
+                    # TODO Can we replace BytesIO() with StringIO for consistency and less
+                    # reading from memory.
+                    bio = BytesIO()
+                    IMG.save(bio, format="JPEG")
+                    bio.seek(0)
+                    AMPLIFIED.append_frame(ORIGINAL.ts[i + indx], bio.read(-1))
+
+                shutil.rmtree(TMP_FLDR)
+                remove("/tmp/vid.mat")
+
+                i = i + k
+                EVENT_NUMBER = EVENT_NUMBER + 1
+            # Skip if nothing for some reason event none of the above applies
+            else:
+                i = i + 1
+
+        _ENG.quit()
+
+        if len(ORIGINAL.ts) != len(AMPLIFIED.timestamps):
+            raise NonMatchingAmplificaton("Length of the files differ")
+        if ORIGINAL.ver != AMPLIFIED.ver:
+            raise NonMatchingAmplificaton("Files have different versions\nOriginal " +
+                                          str(ORIGINAL.ver) + " Amplified " +
+                                          str(AMPLIFIED.ver))
+        AMPLIFIED.check_sanity()
+
+    else:
+        print("Need path to .fif file\nmotion_amplify.py <.fif-file>")
---
+++ PyVideoMEG/matlab_dependencies/amplify.m
@@ -0,0 +1,72 @@
+function amplify(vidFile, sampleCount, framePerSample, cycles)
+
+PhaseBasedAmpDir = '/home/janne/PhaseBasedAmp';
+
+% From PhaseBasedAmp
+% Paths for the linear method
+addpath(fullfile(PhaseBasedAmpDir, 'Linear'));
+addpath(fullfile(PhaseBasedAmpDir, 'Util'));
+addpath(fullfile(PhaseBasedAmpDir, 'matlabPyrTools'));
+addpath(fullfile(PhaseBasedAmpDir, 'matlabPyrTools', 'MEX'));
+
+% Paths for the phase-based method
+addpath(fullfile(PhaseBasedAmpDir, 'PhaseBased'));
+addpath(fullfile(PhaseBasedAmpDir, 'pyrToolsExt'));
+addpath(fullfile(PhaseBasedAmpDir, 'Filters'));
+
+
+
+vr = VideoReader(vidFile);
+fr = vr.FrameRate;
+original = vr.read();
+
+%fr = fps;
+disp('Loading Matrix');
+%original = reshape(vidMatrix, 480,640,3, frameCount);
+%original = load('/tmp/vid.mat');
+[h, w, nChannel, nFrame] = size(original);
+[h,w,nFrame];
+
+frPerSample = framePerSample;
+nSample = sampleCount;
+
+% TODO calculate overflow frames and add them to end
+disp('Slicing video');
+
+samples = zeros(h, w, nChannel, frPerSample, nSample, 'uint8');
+reverse = zeros(h, w, nChannel, frPerSample, nSample,'uint8');
+stich = zeros(h, w, nChannel, frPerSample*2, nSample*cycles,'uint8');
+out = zeros(h, w, nChannel, nSample*frPerSample, 'uint8');
+
+for s = 1:nSample
+    for f = 1:frPerSample
+        samples(:,:,:,f,s) = original(:,:,:,((s-1)*frPerSample)+f);
+        reverse(:,:,:,f,s) = original(:,:,:,(s*(frPerSample+1))-f);
+    end
+    stich(:,:,:,1:frPerSample,s) = samples(:,:,:,:,s);
+    stich(:,:,:,frPerSample+1:2*frPerSample,s) = reverse(:,:,:,:,s);
+
+    disp(sprintf('Amplifying sample %02d/%02d.',s,nSample));
+
+    if cycles ~= 1
+        stich(:,:,:,:,2:cycles) = stich(:,:,:,:,1);
+    end
+
+    % attenuateOtherFreq default is FALSE
+    % sigma default is 0
+    amp = phaseAmplifyMod(stich(:,:,:,:,s), fr , 10, 0.3, 1.3, 50, '', 'sigma', 0, 'attenuateOtherFreq', false, 'temporalFilter', @FIRWindowBP, 'pyrType', 'octave');
+
+    % Resize to allow amplified and original to be side-by-side.
+    % Keep aspect ratio.
+    % Since Elekta Graph expects 640,480 video use those values
+    for f = 1:frPerSample
+        % Uses bicubic interpolation
+        resOrig = imresize(samples(:,:,:,f), [240, 320]);
+        resAmp = imresize(amp(:,:,:,f), [240, 320]);
+        out(121:360,1:320,:,(s-1)*frPerSample+f) = resOrig;
+        out(121:360,321:640,:,(s-1)*frPerSample+f) = resAmp;
+    end
+end
+
+out = im2uint8(out);
+save('/tmp/vid.mat' , 'out');
